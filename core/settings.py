e1dec3e553c777350c6dc98d0f961d0fdb619d80
Bhagath <you@example.com>
2021-01-25 20:45:54 +0530

    for whitenoise

--- core/settings.py
+++ core/settings.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'login_required.middleware.LoginRequiredMiddleware',    # middleware used for global login
 ]
